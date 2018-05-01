29622ad9854d9c6d69af4ac40333411fd8f6e8af
user910210 <user910210@103-246-99-76.moboware.com>
2018-04-30 23:33:10 -0700

    Addition of navigation and swipe out menu

--- Species Identification Guide/ViewController.swift
+++ Species Identification Guide/ViewController.swift
@@ -8,48 +8,80 @@
 
 import UIKit
 
+extension UITextField {
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    func setRightPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
+
 class ViewController: UIViewController {
 
-    @IBOutlet var characteristicList: [UIButton]!
+    
+    @IBOutlet weak var viewConstraint: NSLayoutConstraint!
+    @IBOutlet weak var sideView: UIView!
+    
+    @IBOutlet weak var search: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        search.setLeftPaddingPoints(10)
+        search.setRightPaddingPoints(10)
+        
+        sideView.layer.shadowColor = UIColor.black.cgColor
+        sideView.layer.shadowOpacity = 0.7
+        sideView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        
+        viewConstraint.constant = -210
     }
 
+    @IBAction func panPerformed(_ sender: UIPanGestureRecognizer) {
+        if sender.state == .began || sender.state == .changed {
+            let translation = sender.translation(in: self.view).x
+            if translation > 0 {    // Swipe Right
+                if viewConstraint.constant < 20 {
+                    UIView.animate(withDuration: 0.2, animations: {
+                        self.viewConstraint.constant += translation
+                        self.view.layoutIfNeeded()
+                    })
+                }
+            } else {                // Swipe Left
+                if viewConstraint.constant > -210 {
+                    UIView.animate(withDuration: 0.2, animations: {
+                        self.viewConstraint.constant += translation
+                        self.view.layoutIfNeeded()
+                    })
+                }
+            }
+        } else if sender.state == .ended {
+            if viewConstraint.constant < -100 {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.viewConstraint.constant = -210
+                    self.view.layoutIfNeeded()
+                })
+            } else {
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.viewConstraint.constant = 0
+                    self.view.layoutIfNeeded()
+                })
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func handleSelection(_ sender: UIButton) {
-        characteristicList.forEach { (button) in
-            UIView.animate(withDuration: 0.3, animations: {
-                button.isHidden = !button.isHidden
-                self.view.layoutIfNeeded()
-            })
-        }
-        
-    }
-    
-    enum Characteristics: String {
-        case leg = "Leg"
-        case arm = "Arm"
-        case head = "Head"
-    }
-    
-    @IBAction func characterisitcTapped(_ sender: UIButton) {
-        guard let title = sender.currentTitle, let characteristic = Characteristics(rawValue: title) else {
-            return
-        }
-        
-        switch characteristic {
-        case .leg:
-            print("Leg")
-        case .arm:
-            print("Arm")
-        default:
-            print("Default")
-        }
-    }
 }
+
+
+
+
