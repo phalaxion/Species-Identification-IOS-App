8d8870712f1cbdc731fb51cc538d12e0e8b4538b
user910210 <user910210@103-246-99-76.moboware.com>
2018-04-30 20:35:40 -0700

    Dropdown Menu Foundation

--- Species Identification Guide/ViewController.swift
+++ Species Identification Guide/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var characteristicList: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func handleSelection(_ sender: UIButton) {
+        characteristicList.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
+        
+    }
+    
+    enum Characteristics: String {
+        case leg = "Leg"
+        case arm = "Arm"
+        case head = "Head"
+    }
+    
+    @IBAction func characterisitcTapped(_ sender: UIButton) {
+        guard let title = sender.currentTitle, let characteristic = Characteristics(rawValue: title) else {
+            return
+        }
+        
+        switch characteristic {
+        case .leg:
+            print("Leg")
+        case .arm:
+            print("Arm")
+        default:
+            print("Default")
+        }
+    }
 }
-
