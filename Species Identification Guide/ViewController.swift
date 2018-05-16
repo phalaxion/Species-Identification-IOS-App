26b5d2271416faab48f378dd62704590850e7627
Cyrus Ng <pengu213456@gmail.com>
2018-05-16 13:22:57 +1000

    Added all categories relating to legs as well as wing number/texture, minor issues still exist from previous

--- Species Identification Guide/ViewController.swift
+++ Species Identification Guide/ViewController.swift
@@ -203,7 +203,6 @@ class ViewController: UIViewController {
     
     @IBAction func bodyCompressionTapped(_ sender: UIButton) {
         guard let title = sender.currentTitle, let compression = BodyCompressions(rawValue: title) else {
-            print("nooo")
             return
         }
         switch compression{
@@ -230,6 +229,156 @@ class ViewController: UIViewController {
         }
         else {
             abdomenThoraxConstriction.setTitle("Does Not Have Constriction Between Thorax and Abdomen", for: .normal)
+        }
+    }
+    
+    @IBOutlet weak var numLegsButton: UIButton!
+    @IBOutlet var numLegs: [UIButton]!
+    @IBAction func numLegChoice(_ sender: UIButton) {
+        numLegs.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    enum NumLegs: String {
+        case zero = "0 Legs"
+        case six = "6 Legs"
+        case eight = "8 Legs"
+        case eightPlus = "More than 8 Legs"
+    }
+    @IBAction func numLegsTapped(_ sender: UIButton) {
+        guard let title = sender.currentTitle, let shape = NumLegs(rawValue: title) else {
+            return
+        }
+        switch shape{
+        case .zero:
+            numLegsButton.setTitle("0 Legs", for: .normal)
+            numLegChoice(sender)
+        case .six:
+            numLegsButton.setTitle("6 Legs", for: .normal)
+            numLegChoice(sender)
+        case .eight:
+            numLegsButton.setTitle("8 Legs", for: .normal)
+            numLegChoice(sender)
+        case .eightPlus:
+            numLegsButton.setTitle("More than 8 Legs", for: .normal)
+            numLegChoice(sender)
+        }
+    }
+    
+    @IBOutlet weak var legTypeButton: UIButton!
+    @IBOutlet var legTypes: [UIButton]!
+    
+    @IBAction func legTypeChoice(_ sender: UIButton) {
+        legTypes.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    enum LegTypes: String {
+        case similar = "All look very similar (and similar length)"
+        case different = "Legs different lengths"
+        case jumpingHind = "Strong jumping hind legs"
+        case raptorial = "Raptorial (like a T-rex)"
+        case popeye = "Looks a bit like Popeye's arms"
+    }
+    @IBAction func legTypeTapped(_ sender: UIButton) {
+        guard let title = sender.currentTitle, let legType = LegTypes(rawValue: title) else {
+            return
+        }
+        switch legType{
+        case .similar:
+            legTypeButton.setTitle("All look very similar (and similar length)", for: .normal)
+            legTypeChoice(sender)
+        case .different:
+            legTypeButton.setTitle("Legs different lengths", for: .normal)
+            legTypeChoice(sender)
+        case .jumpingHind:
+            legTypeButton.setTitle("Strong jumping hind legs", for: .normal)
+            legTypeChoice(sender)
+        case .raptorial:
+            legTypeButton.setTitle("Raptorial (like a T-rex)", for: .normal)
+            legTypeChoice(sender)
+        case .popeye:
+            legTypeButton.setTitle("Looks a bit like Popeye's arms", for: .normal)
+            legTypeChoice(sender)
+        }
+    }
+    
+    @IBOutlet weak var numWingsButton: UIButton!
+    @IBOutlet var numWings: [UIButton]!
+    @IBAction func numWingsChoice(_ sender: UIButton) {
+        numWings.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    enum NumWings: String {
+        case none = "No Wings"
+        case two = "2 Wings (+a pair of halteres or elytra)"
+        case four = "4 Wings"
+    }
+    @IBAction func numWingsTapped(_ sender: UIButton) {
+        guard let title = sender.currentTitle, let numWing = NumWings(rawValue: title) else {
+            return
+        }
+        switch numWing{
+        case .none:
+            numWingsButton.setTitle("No Wings", for: .normal)
+            numWingsChoice(sender)
+        case .two:
+            numWingsButton.setTitle("2 Wings (+a pair of halteres or elytra)", for: .normal)
+            numWingsChoice(sender)
+        case .four:
+            numWingsButton.setTitle("4 Wings", for: .normal)
+            numWingsChoice(sender)
+        }
+    }
+    
+    @IBOutlet weak var wingTextureButton: UIButton!
+    @IBOutlet var wingTextures: [UIButton]!
+    
+    @IBAction func wingTextureChoice(_ sender: UIButton) {
+        wingTextures.forEach { (button) in
+            UIView.animate(withDuration: 0.3, animations: {
+                button.isHidden = !button.isHidden
+                self.view.layoutIfNeeded()
+            })
+        }
+    }
+    enum WingTextures: String {
+        case hairy = "Hairy (all over or on edges / margins)"
+        case scaly = "Scaly and Patterned (like a Butterfly)"
+        case membranous0 = "Membranous"
+        case membranous1 = "Membranous (highly complex vein pattern)"
+        case membranous2 = "Membranous (simple vein pattern)"
+    }
+    @IBAction func wingTextureTapped(_ sender: UIButton) {
+        guard let title = sender.currentTitle, let wingTexture = WingTextures(rawValue: title) else {
+            return
+        }
+        switch wingTexture{
+        case .hairy:
+            wingTextureButton.setTitle("Hairy (all over or on edges / margins", for: .normal)
+            wingTextureChoice(sender)
+        case .scaly:
+            wingTextureButton.setTitle("Scaly and Patterned (like a Butterfly)", for: .normal)
+            wingTextureChoice(sender)
+        case .membranous0:
+            wingTextureButton.setTitle("Membranous", for: .normal)
+            wingTextureChoice(sender)
+        case .membranous1:
+            wingTextureButton.setTitle("Membranous (highly complex vein pattern)", for: .normal)
+            wingTextureChoice(sender)
+        case .membranous2:
+            wingTextureButton.setTitle("Membranous (simple vein pattern)", for: .normal)
+            wingTextureChoice(sender)
         }
     }
 }
